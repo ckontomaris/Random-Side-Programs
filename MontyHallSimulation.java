29dabb0f65f4becef918bc762f4c504cf9abb682
Chris Kontomaris <32843874+ckontomaris@users.noreply.github.com>
2018-03-06 18:51:16 -0500

    added monty hall simulation file

---
+++ MontyHallSimulation.java
@@ -0,0 +1,18 @@
+/**
+* Runs a simulation of the Monty Hall Problem to verify its solution
+* @author Chris Kontomaris
+* @version 3/6/18
+*/
+
+
+import java.io.*;
+import java.util.*;
+
+public class MontyHallSimulation{
+    public static void main(String [] args){
+    //if we change our guess
+
+    //if we don't change our guess    
+
+    }
+}
